8aa233830a8271750d635e6a066a7ccf46d99c57
bouskriKarim <bousskrikarim47@gmail.com>
2023-07-21 04:04:32 +0100

    feat : add service section

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function showElement(id){
+    let element = document.getElementById(id);
+    element.style.display = "block";
+}
+
+function hideElement(id){
+    let element = document.getElementById(id);
+    element.style.display = "none";
+}
